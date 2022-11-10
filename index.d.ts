c01b61adca48c3ce4cfca5b3a7c3f480564f050f
Ryan Shaw <ryankshaw@gmail.com>
2022-11-09 21:02:00 -0700

    add types

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Ensures environment variable exists and returns it
+ * @param {string} name Name of environment variable you want to make sure exists
+ * @returns {string} the value of that environment variable
+ */
+export default function requiredEnvVar(name: string): string;
